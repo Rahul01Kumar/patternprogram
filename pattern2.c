ff41080b6758065a59bcc63a5320be6685231a12
RAHUL KUMAR <114794076+Rahul01Kumar@users.noreply.github.com>
2023-03-01 23:03:24 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=n;i>=0;i--)//Row control or line control
+	{
+		for(j=1;j>=n;j--)// printed value control loop
+		{
+			printf("%d",i);
+		
+		}	printf("\n");
+	}
+}
+
