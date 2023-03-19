b26f2c74283e762b31f89fd5f7e037b6275d15dc
RAHUL KUMAR <114794076+Rahul01Kumar@users.noreply.github.com>
2023-03-19 14:17:03 +0530

    Create pattern3.c

---
+++ pattern3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+  int i,j,input;
+  printf("ENter the no of lines = ");
+  scanf("%d",&input);
+  for(i=1;i<=input;i++){
+    for(j=1;j<=i;j++){
+    printf("*");}
+  }printf("\n");
+}
